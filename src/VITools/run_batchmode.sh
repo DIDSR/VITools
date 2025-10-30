aa78a45a8ad0b76a9403e1c09019ea145e3997fb
Brandon J Nelson <brandon.nelson@fda.hhs.gov>
2025-10-30 15:07:09 -0400

    slurm task id matches case id (+1) in case of chunked batches where case_id doesnt start at 0

--- src/VITools/run_batchmode.sh
+++ src/VITools/run_batchmode.sh
@@ -7,11 +7,13 @@ LOG_DIR=${4:-"./.VIT/logs/$SIM_NAME"}
 
 COUNT=$(cat $INPUT | wc -l)
 COUNT=$(($COUNT - 1))
-echo Running $COUNT simulation conditions
+FIRST_ID=$(awk -F ',' 'NR==2 {split($1, a, "_"); print a[2]+0}' "$INPUT")
+LAST_ID=$(awk -F ',' 'END {split($1, a, "_"); print a[2]+0}' "$INPUT")
+echo Running $COUNT simulation conditions: $FIRST_ID-$LAST_ID
 
 absolute_path=$(realpath "$LOG_DIR")
 echo "See logs at $absolute_path"
 
-START_TASK=1
-END_TASK=$COUNT
-qsub -N $SIM_NAME -t $START_TASK-$END_TASK $CMD_FILE $PY_ENV $LOG_DIR $COUNT $INPUT
+START_TASK=$FIRST_ID
+END_TASK=$LAST_ID
+qsub -N $SIM_NAME -t $START_TASK-$END_TASK $CMD_FILE $PY_ENV $LOG_DIR $INPUT
